ee5a20c68c65012b1dba78e6fe66d471dcc61927
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-02 21:27:12 +0200

    feat: Create navbar menu for the app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Navbar from "@/components/navbar/navbar";
 
 import "./globals.css";
 
@@ -14,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
