bc1028ea266bdec64215bd2e51386a72969c8095
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-08 23:08:14 +0200

    feat: Create main page of the app.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,45 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import MaintentanceInfoCard from "@/components/maintenance-info-card/maintenance-info-card";
+import VehicleCard from "@/components/vehicle-card/vehicle-card";
 
 export default function Home() {
   return (
     <div className="flex flex-col lg:flex-row h-screen">
       {/* Left Section */}
-      <div className="lg:w-1/2 w-full bg-blue-500 p-6 flex items-center justify-center text-white text-2xl">
-        Left Section
+      <div className="lg:w-1/2 w-full p-6 flex text-white text-2xl">
+        <VehicleCard />
       </div>
 
       {/* Right Section (Stacked) */}
       <div className="lg:w-1/2 w-full flex flex-col">
-        <div className="h-1/2 bg-green-500 p-6 flex items-center justify-center text-white text-2xl">
-          Top Right
+        <div className="p-6 flex items-center justify-center text-white text-2xl">
+          <MaintentanceInfoCard
+            icon=""
+            title="Service"
+            firstLabel="Τελευταίο Service"
+            firstValue="2025-01-01"
+            secondLabel="Επόμενο Service"
+            secondValue="2022-01-01"
+          />
         </div>
-        <div className="h-1/2 bg-red-500 p-6 flex items-center justify-center text-white text-2xl">
-          Bottom Right
+        <div className="p-6 flex items-center justify-center text-white text-2xl">
+          <MaintentanceInfoCard
+            icon=""
+            title="ΚΤΕΟ"
+            firstLabel="Τελευταίο KTEO"
+            firstValue="2025-01-01"
+            secondLabel="Επόμενο KTEO"
+            secondValue="2022-01-01"
+          />
+        </div>
+        <div className="p-6 flex items-center justify-center text-white text-2xl">
+          <MaintentanceInfoCard
+            icon=""
+            title="Ασφάλεια"
+            firstLabel="Εταιρία"
+            firstValue="Generali"
+            secondLabel="Επόμενη Ανανέωση"
+            secondValue="2022-01-01"
+          />
         </div>
       </div>
     </div>
