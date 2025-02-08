4533c774b04e3592e4bc2d6a8196d252a56a8354
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-08 22:31:24 +0200

    feat: Add logo of car brand to card header.

--- components/vehicle-card/vehicle-card.tsx
+++ components/vehicle-card/vehicle-card.tsx
@@ -1,14 +1,20 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import GeneralInfo from "./general-info";
 import EngineDetails from "./engine-details";
+import Image from "next/image";
 
 export default function VehicleCard() {
   return (
-    <Card className="w-full lg:max-w-[1000px] max-h-[870px] md:max-h-[550px]">
+    <Card className="w-full lg:max-w-[1000px] max-h-[970px] md:max-h-[750px]">
       <CardHeader>
-        <CardTitle>
+        <CardTitle className="flex items-center gap-4">
+          <Image
+            src="https://www.carlogos.org/logo/Opel-logo.png"
+            alt="Opel Logo"
+            width={100}
+            height={100}
+          />
           Opel
-          <div className="h-5 border-b-2 border-blue text-2xl text-center"></div>
         </CardTitle>
       </CardHeader>
       <CardContent>
