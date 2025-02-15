c30cbc6be78df88c055e55db7452a087f3656c57
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-15 22:59:26 +0200

    feat: Create comments component.

---
+++ components/comments/comments.tsx
@@ -0,0 +1,15 @@
+export default function Comments() {
+  return (
+    <div className="bg-gray-200 rounded-lg p-4 mx-auto">
+      <h1 className="text-xl font-bold text-center text-black-600">
+        Παρατηρήσεις
+      </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis
+        harum neque quod voluptatum sequi repellendus, odio delectus rem nemo
+        laborum assumenda, dolorum sed voluptatem deserunt quos. Nam fugiat
+        asperiores quae.
+      </p>
+    </div>
+  );
+}
