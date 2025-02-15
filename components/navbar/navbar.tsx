02ff165bfa4f43e3063b11f090f65533f49244f3
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-15 15:39:49 +0200

    fix: Add links to navbar.

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -13,6 +13,9 @@ export default function Navbar() {
   const sheetStyles =
     "group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50";
 
+  const sheetStylesMobile =
+    "flex w-full items-center py-2 text-lg font-semibold";
+
   return (
     <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
       <Sheet>
@@ -30,32 +33,24 @@ export default function Navbar() {
             <Car className="h-6 w-6" />
           </Link>
           <div className="grid gap-2 py-6">
-            <Link
-              href="/"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
+            <Link href="/" className={sheetStylesMobile} prefetch={false}>
               Αρχική
             </Link>
             <Link
               href="/services"
-              className="flex w-full items-center py-2 text-lg font-semibold"
+              className={sheetStylesMobile}
               prefetch={false}
             >
               Τα service μου
             </Link>
             <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="/insurance"
+              className={sheetStylesMobile}
               prefetch={false}
             >
               Οι ασφάλειές μου
             </Link>
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
+            <Link href="/kteo" className={sheetStylesMobile} prefetch={false}>
               Τα ΚΤΕΟ μου
             </Link>
           </div>
@@ -71,10 +66,10 @@ export default function Navbar() {
         <Link href="/services" className={sheetStyles} prefetch={false}>
           Τα service μου
         </Link>
-        <Link href="#" className={sheetStyles} prefetch={false}>
+        <Link href="/insurance" className={sheetStyles} prefetch={false}>
           Οι ασφάλειές μου
         </Link>
-        <Link href="#" className={sheetStyles} prefetch={false}>
+        <Link href="/kteo" className={sheetStyles} prefetch={false}>
           Τα ΚΤΕΟ μου
         </Link>
       </nav>
