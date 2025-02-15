82f06f753c241b84b07380ddfd09f591c6081132
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-15 22:59:45 +0200

    feat: Create details sheet at service section.

--- components/maintenance-info-card/maintenance-info-card-horizontal.tsx
+++ components/maintenance-info-card/maintenance-info-card-horizontal.tsx
@@ -1,7 +1,18 @@
-import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
-import { Wrench, Shield, Book } from "lucide-react";
-import { Badge } from "../ui/badge";
+"use client";
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+import Comments from "../comments/comments";
 
 type Title = "Service" | "Ασφάλεια" | "ΚΤΕΟ";
 
@@ -44,7 +55,61 @@ export default function MaintentanceInfoCardHorizontal({
               <td className="text-lg p-2 break-words">{thirdValue}</td>
             )}
             <td className="text-lg p-2 break-words">
-              <Button>Λεπτομέρειες</Button>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button>Λεπτομέρειες</Button>
+                </SheetTrigger>
+                <SheetContent
+                  className={cn("w-full sm:max-w-[600px] overflow-y-auto")}
+                >
+                  <SheetHeader>
+                    <SheetTitle>{title}</SheetTitle>
+                  </SheetHeader>
+                  <div className="grid gap-4 py-8">
+                    <div className="grid grid-cols-2 items-center">
+                      <p>Ημ/νία Ελέγχου:</p>
+                      <p>Χιλιόμετρα:</p>
+                    </div>
+                    <div className="grid grid-cols-2 items-center">
+                      <p>2025/12/02</p>
+                      <p>20.000 km/h</p>
+                    </div>
+                    <div className="border-b-2 border-blue text-2xl text-center"></div>
+                    <div className="grid grid-cols-3 items-center">
+                      <p>Τύπος Service:</p>
+                      <p>Τακτικό</p>
+                    </div>
+                    <div className="grid grid-cols-3 items-center">
+                      <p>Έλεγχοι:</p>
+                      <ul>
+                        <li>Λάδια</li>
+                        <li>Φίλτρο Λαδιού</li>
+                        <li>Φίλτρο Αέρα</li>
+                        <li>Φίλτρο Καμπίνας</li>
+                      </ul>
+                    </div>
+                    <Comments />
+                    <div className="grid grid-cols-3 items-center">
+                      <p>Κόστος:</p>
+                      <p>120€</p>
+                    </div>
+                    <div className="border-b-2 border-blue text-2xl text-center"></div>
+                    <div className="grid grid-cols-2 items-center">
+                      <p>Επόμενο Service:</p>
+                      <p>Ημ/νία Επόμενου Service:</p>
+                    </div>
+                    <div className="grid grid-cols-2 items-center">
+                      <p>σε 20.000 km/h</p>
+                      <p>2026/22/12</p>
+                    </div>
+                  </div>
+                  <SheetFooter>
+                    <SheetClose asChild>
+                      <Button type="submit">Επεξεργασία</Button>
+                    </SheetClose>
+                  </SheetFooter>
+                </SheetContent>
+              </Sheet>
             </td>
           </tr>
         </tbody>
