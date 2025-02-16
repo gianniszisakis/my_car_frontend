872fba0901f8b5801aac08640323d407f410991a
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-16 22:07:54 +0200

    feat: Add 2 more columns in service card.

--- components/maintenance-info-card/maintenance-info-card-horizontal.tsx
+++ components/maintenance-info-card/maintenance-info-card-horizontal.tsx
@@ -14,12 +14,13 @@ export default function MaintentanceInfoCardHorizontal({
 }: MaintentanceInfoCardHorizontalProps) {
   return (
     <div className="p-6 w-full overflow-x-auto">
-      <table className="w-[600px] md:w-full table-auto border-collapse border border-gray-300">
+      <table className="w-[800px] md:w-full table-auto border-collapse border border-gray-300">
         <thead>
           <tr className="bg-gray-200">
             <th className="p-2 text-left">Τελευταίο Service</th>
-            <th className="p-2 text-left">Επόμενο Service</th>
-            {/*{thirdLabel && <th className="p-2 text-left">{thirdLabel}</th>}*/}
+            <th className="p-2 text-left">Επόμενο Service (Ημ/νία)</th>
+            <th className="p-2 text-left">Επόμενο Service (km/h)</th>
+            <th className="p-2 text-left">Τύπος</th>
             <th className="p-2 text-left"></th>
           </tr>
         </thead>
@@ -33,6 +34,14 @@ export default function MaintentanceInfoCardHorizontal({
                 {service?.next_service?.date
                   ? service?.next_service?.date
                   : "-"}
+              </td>
+              <td className="text-lg p-2 break-words">
+                {service?.next_service?.mileage_km
+                  ? service?.next_service?.mileage_km
+                  : "-"}
+              </td>
+              <td className="text-lg p-2 break-words">
+                {service?.service_type ? service?.service_type : "-"}
               </td>
               <td className="text-lg p-2 break-words">
                 <ServiceDetailsSheet service={service} />
