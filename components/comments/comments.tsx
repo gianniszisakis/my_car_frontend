23d2915505834a0041ceaccf3a857b638733b9de
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-16 21:42:16 +0200

    fix: Comments as a prop.

--- components/comments/comments.tsx
+++ components/comments/comments.tsx
@@ -1,15 +1,13 @@
-export default function Comments() {
+interface CommentsProps {
+  comments: string;
+}
+export default function Comments({ comments }: CommentsProps) {
   return (
     <div className="bg-gray-200 rounded-lg p-4 mx-auto">
       <h1 className="text-xl font-bold text-center text-black-600">
         Παρατηρήσεις
       </h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis
-        harum neque quod voluptatum sequi repellendus, odio delectus rem nemo
-        laborum assumenda, dolorum sed voluptatem deserunt quos. Nam fugiat
-        asperiores quae.
-      </p>
+      <p className="w-[300px] md:w-[500px] text-center">{comments}</p>
     </div>
   );
 }
