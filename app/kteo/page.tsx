6829cafd6e2ed45a5dc295b026b56b1483e087d7
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-17 19:16:08 +0200

    feat: Render all kteo table.

--- app/kteo/page.tsx
+++ app/kteo/page.tsx
@@ -1,4 +1,5 @@
-import MaintentanceInfoCardHorizontal from "@/components/services/all-services-table";
+import AllKteoTable from "@/components/kteo/all-kteo-table";
+import vehicleData from "@/public/placeholder-data/vehicleData";
 
 export default function KTEO() {
   return (
@@ -7,15 +8,9 @@ export default function KTEO() {
         Τα ΚΤΕΟ μου
       </h1>
       <div className="flex flex-row h-screen">
-        <MaintentanceInfoCardHorizontal
-          title="Service"
-          firstLabel="Τελευταίο KTEO"
-          firstValue="2025-01-01"
-          secondLabel="Επόμενο KTEO"
-          secondValue="2022-01-01"
-          thirdLabel="ΚΤΕΟ"
-          thirdValue="AUTOVISION"
-        />
+        {vehicleData?.kteo.length > 0 ? (
+          <AllKteoTable allKteo={vehicleData?.kteo} />
+        ) : null}
       </div>
     </>
   );
