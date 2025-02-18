88310f76196630c47f42b1705e174ffed9bfbcf6
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-18 15:32:31 +0200

    fix: Badge color from function.

--- components/insurance/all-insurances-table.tsx
+++ components/insurance/all-insurances-table.tsx
@@ -26,44 +26,49 @@ export default function AllInsurancesTable({
           </tr>
         </thead>
         <tbody>
-          {insurances?.map((insurance) => (
-            <tr
-              className="border-b border-gray-300"
-              key={insurance?.insurance_id}
-            >
-              <td className="text-lg p-2 break-words">
-                {insurance?.last_insurance_date
-                  ? insurance?.last_insurance_date
-                  : "-"}
-              </td>
-              <td className="text-lg p-2 break-words">
-                {insurance?.next_renewal_date
-                  ? insurance?.next_renewal_date
-                  : "-"}
-              </td>
-              <td className="text-lg p-2 break-words">
-                {insurance?.insurance_type ? insurance?.insurance_type : "-"}
-              </td>
-              <td className="text-lg p-2 break-words">
-                {insurance?.insurance_company
-                  ? insurance?.insurance_company
-                  : "-"}
-              </td>
-              <td className="text-lg p-2 break-words">
-                <Badge
-                  variant="outline"
-                  className={`${getStatusBadgeColor(
-                    insurance?.next_renewal_date
-                  )} text-white pb-1 pt-1`}
-                >
-                  {getExpirationStatus(insurance?.next_renewal_date)}
-                </Badge>
-              </td>
-              <td className="text-lg p-2 break-words">
-                <ServiceDetailsSheet insurance={insurance} />
-              </td>
-            </tr>
-          ))}
+          {insurances?.map((insurance) => {
+            const status = getExpirationStatus(insurance?.next_renewal_date);
+            const badgeColor = getStatusBadgeColor(
+              insurance?.next_renewal_date
+            );
+            //console.log(`status ${status} and color ${badgeColor}`);
+            return (
+              <tr
+                className="border-b border-gray-300"
+                key={insurance?.insurance_id}
+              >
+                <td className="text-lg p-2 break-words">
+                  {insurance?.last_insurance_date
+                    ? insurance?.last_insurance_date
+                    : "-"}
+                </td>
+                <td className="text-lg p-2 break-words">
+                  {insurance?.next_renewal_date
+                    ? insurance?.next_renewal_date
+                    : "-"}
+                </td>
+                <td className="text-lg p-2 break-words">
+                  {insurance?.insurance_type ? insurance?.insurance_type : "-"}
+                </td>
+                <td className="text-lg p-2 break-words">
+                  {insurance?.insurance_company
+                    ? insurance?.insurance_company
+                    : "-"}
+                </td>
+                <td className="text-lg p-2 break-words">
+                  <Badge
+                    variant="outline"
+                    className={`${badgeColor} text-white pb-1 pt-1`}
+                  >
+                    {status}
+                  </Badge>
+                </td>
+                <td className="text-lg p-2 break-words">
+                  <ServiceDetailsSheet insurance={insurance} />
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
