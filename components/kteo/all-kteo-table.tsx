00f4ff56f99a73403dc5f3a0d9b94c5bc4f3b9ad
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-18 15:33:21 +0200

    fix: Badge color from function.

--- components/kteo/all-kteo-table.tsx
+++ components/kteo/all-kteo-table.tsx
@@ -22,29 +22,33 @@ export default function AllKteoTable({ allKteo }: AllKteoTableProps) {
           </tr>
         </thead>
         <tbody>
-          {allKteo?.map((kteo) => (
-            <tr className="border-b border-gray-300" key={kteo?.kteo_id}>
-              <td className="text-lg p-2 break-words">
-                {kteo?.kteo_last_date ? kteo?.kteo_last_date : "-"}
-              </td>
-              <td className="text-lg p-2 break-words">
-                {kteo?.kteo_next_date ? kteo?.kteo_next_date : "-"}
-              </td>
-              <td className="text-lg p-2 break-words">
-                <Badge
-                  variant="outline"
-                  className={`${getStatusBadgeColor(
-                    kteo?.kteo_next_date
-                  )} text-white pb-1 pt-1`}
-                >
-                  {getExpirationStatus(kteo?.kteo_next_date)}
-                </Badge>
-              </td>
-              <td className="text-lg p-2 break-words">
-                <ServiceDetailsSheet kteo={kteo} />
-              </td>
-            </tr>
-          ))}
+          {allKteo?.map((kteo) => {
+            const status = getExpirationStatus(kteo?.kteo_next_date);
+            //console.log("status", status);
+            return (
+              <tr className="border-b border-gray-300" key={kteo?.kteo_id}>
+                <td className="text-lg p-2 break-words">
+                  {kteo?.kteo_last_date ? kteo?.kteo_last_date : "-"}
+                </td>
+                <td className="text-lg p-2 break-words">
+                  {kteo?.kteo_next_date ? kteo?.kteo_next_date : "-"}
+                </td>
+                <td className="text-lg p-2 break-words">
+                  <Badge
+                    variant="outline"
+                    className={`${getStatusBadgeColor(
+                      kteo?.kteo_next_date
+                    )} text-white pb-1 pt-1`}
+                  >
+                    {getExpirationStatus(kteo?.kteo_next_date)}
+                  </Badge>
+                </td>
+                <td className="text-lg p-2 break-words">
+                  <ServiceDetailsSheet kteo={kteo} />
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
