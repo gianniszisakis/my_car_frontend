6619eecf328324eddf0defb2191b962955fc7b7c
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-24 17:00:20 +0200

    feat: Add Vehicle interface model to vehicle data.

--- hooks/useVehicleData.ts
+++ hooks/useVehicleData.ts
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "@/lib/axios";
+import { Vehicle } from "@/models/vehicleModel";
 
-async function fetchVehicle() {
-  const { data } = await api.get("/vehicle");
+async function fetchVehicle(): Promise<Vehicle> {
+  const { data } = await api.get<Vehicle>("/vehicle");
   return data;
 }
 
 export function useVehicleData() {
-  return useQuery({
+  return useQuery<Vehicle>({
     queryKey: ["vehicleData"],
     queryFn: fetchVehicle,
     staleTime: 1000 * 60 * 30, // Cache valid for 30 minutes
