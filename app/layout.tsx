6774f523994fff16bdd0384e2d69a18e4de4ec22
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-24 16:33:07 +0200

    feat: Setup React Query Provider.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,29 @@
-import type { Metadata } from "next";
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
 import Navbar from "@/components/navbar/navbar";
 
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "My Car",
-  description: "Every action about your car maintenance",
-};
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // Ensure the QueryClient persists across re-renders
+  const [client] = useState(() => new QueryClient());
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        <main>{children}</main>
+        <QueryClientProvider client={client}>
+          <Navbar />
+          <main>{children}</main>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </body>
     </html>
   );
