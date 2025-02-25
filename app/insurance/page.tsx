236aa2d21601e0999ffa58be8230f4139212b097
Ioannis ZISAKIS <giannis.zisakis@gmail.com>
2025-02-25 14:13:35 +0200

    feat: Map data from API.

--- app/insurance/page.tsx
+++ app/insurance/page.tsx
@@ -1,16 +1,30 @@
+"use client";
 import AllInsurancesTable from "@/components/insurance/all-insurances-table";
-import vehicleData from "@/public/placeholder-data/vehicleData";
+import { LoadingPageEntities } from "@/components/loading-error/loading-cards-pages";
+import NoDataErrorPages from "@/components/loading-error/no-data-error-pages";
+import NewEventButton from "@/components/new-event/new-event-button";
+import { useAllInsurance } from "@/hooks/useAllInsurance";
 
 export default function Insurance() {
+  const { data, isLoading, error } = useAllInsurance();
+  if (isLoading) {
+    return <LoadingPageEntities />;
+  }
+  if (error) {
+    return <NoDataErrorPages isError={true} />;
+  }
   return (
     <>
       <h1 className="p-6 text-3xl font-bold text-center text-black-600 w-full">
         Οι ασφάλειές μου
       </h1>
+      <NewEventButton text="Νέα ασφάλεια" targetLink="/kteo/newKteo/" />
       <div className="flex flex-row h-screen">
-        {vehicleData.insurance.length > 0 ? (
-          <AllInsurancesTable insurances={vehicleData?.insurance} />
-        ) : null}
+        {!data?.length ? (
+          <NoDataErrorPages noData={!data?.length} />
+        ) : (
+          <AllInsurancesTable insurances={data} />
+        )}
       </div>
     </>
   );
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,16 +1,30 @@
+"use client";
+import { LoadingPageEntities } from "@/components/loading-error/loading-cards-pages";
+import NoDataErrorPages from "@/components/loading-error/no-data-error-pages";
+import NewEventButton from "@/components/new-event/new-event-button";
 import AllServicesTable from "@/components/services/all-services-table";
-import vehicleData from "@/public/placeholder-data/vehicleData";
+import { useAllService } from "@/hooks/useAllServices";
 
 export default function Services() {
+  const { data, isLoading, error } = useAllService();
+  if (isLoading) {
+    return <LoadingPageEntities />;
+  }
+  if (error) {
+    return <NoDataErrorPages isError={true} />;
+  }
   return (
     <>
       <h1 className="p-6 text-3xl font-bold text-center text-black-600 w-full">
         Τα service μου
       </h1>
+      <NewEventButton text="Νέο service" targetLink="/kteo/newKteo/" />
       <div className="flex flex-row h-screen">
-        {vehicleData.services.length > 0 ? (
-          <AllServicesTable services={vehicleData?.services} />
-        ) : null}
+        {!data?.length ? (
+          <NoDataErrorPages noData={!data?.length} />
+        ) : (
+          <AllServicesTable services={data} />
+        )}
       </div>
     </>
   );
