d7955eb5b9bf4130a3967130096eed7f32bcbfb3
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2025-03-27 15:15:43 +0200

    feat: Show welcome message if user name exists.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import NewCarForm from "@/components/forms/new-car-form";
 import VehicleMaintenanceData from "@/components/main-page/vehicle-maintenance-data";
-import vehicleData from "@/public/placeholder-data/vehicleData";
 import { useVehicleData } from "@/hooks/useVehicleData";
 import { SkeletonLoadingPage } from "@/components/loading-error/loading-cards";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data, isLoading, error } = useVehicleData();
