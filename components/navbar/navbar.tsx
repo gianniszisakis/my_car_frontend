4a4b9eaddde751eb71a601633ae7131b8615a245
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2025-03-27 16:02:13 +0200

    feat: Insert avatar in navbar.

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -8,8 +8,13 @@ import {
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Menu, Car } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useSession } from "next-auth/react";
 
 export default function Navbar() {
+  //Get user data from session
+  const { data: session, status } = useSession();
+
   const sheetStyles =
     "group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50";
 
@@ -29,6 +34,16 @@ export default function Navbar() {
           <SheetDescription>
             Διαχειριστείτε όλες τις ενέργειες του αυτοκινήτου σας.
           </SheetDescription>
+          <div className="pt-4 flex flex-row items-center">
+            <Avatar>
+              <AvatarImage src={session?.user?.image ?? ""} />
+              <AvatarFallback>NA</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <p className="pl-4 text-[12px]">{session?.user?.name ?? "-"}</p>
+              <p className="pl-4 text-[12px]">{session?.user?.email ?? "-"}</p>
+            </div>
+          </div>
           <Link href="/" className="mr-6 hidden lg:flex" prefetch={false}>
             <Car className="h-6 w-6" />
           </Link>
@@ -72,6 +87,10 @@ export default function Navbar() {
         <Link href="/kteo" className={sheetStyles} prefetch={false}>
           Τα ΚΤΕΟ μου
         </Link>
+        <Avatar>
+          <AvatarImage src={session?.user?.image ?? ""} />
+          <AvatarFallback>NA</AvatarFallback>
+        </Avatar>
       </nav>
     </header>
   );
