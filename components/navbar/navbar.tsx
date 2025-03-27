dc67ace4ac757c5e9a1d0a51cfcd07a32d8e5105
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2025-03-27 16:41:05 +0200

    feat: Create user avatar component, add signout in mobile menu.

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -5,25 +5,13 @@ import {
   SheetTitle,
   SheetDescription,
 } from "@/components/ui/sheet";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Menu, Car } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useSession } from "next-auth/react";
+import { UserAvatar, MobileUserAvatar } from "./user-avatar";
+import { signOut } from "next-auth/react";
 
 export default function Navbar() {
-  //Get user data from session
-  const { data: session, status } = useSession();
-
   const sheetStyles =
     "group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50";
 
@@ -43,16 +31,7 @@ export default function Navbar() {
           <SheetDescription>
             Διαχειριστείτε όλες τις ενέργειες του αυτοκινήτου σας.
           </SheetDescription>
-          <div className="pt-4 flex flex-row items-center">
-            <Avatar>
-              <AvatarImage src={session?.user?.image ?? ""} />
-              <AvatarFallback>NA</AvatarFallback>
-            </Avatar>
-            <div className="flex flex-col">
-              <p className="pl-4 text-[12px]">{session?.user?.name ?? "-"}</p>
-              <p className="pl-4 text-[12px]">{session?.user?.email ?? "-"}</p>
-            </div>
-          </div>
+          <MobileUserAvatar />
           <Link href="/" className="mr-6 hidden lg:flex" prefetch={false}>
             <Car className="h-6 w-6" />
           </Link>
@@ -77,6 +56,14 @@ export default function Navbar() {
             <Link href="/kteo" className={sheetStylesMobile} prefetch={false}>
               Τα ΚΤΕΟ μου
             </Link>
+            <Link
+              href="/profile"
+              className={sheetStylesMobile}
+              prefetch={false}
+            >
+              Τα προφίλ μου
+            </Link>
+            <Button onClick={() => signOut()}>Αποσύνδεση</Button>
           </div>
         </SheetContent>
       </Sheet>
@@ -97,24 +84,7 @@ export default function Navbar() {
           Τα ΚΤΕΟ μου
         </Link>
       </nav>
-      <div className="pr-4 hidden lg:flex">
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Avatar>
-              <AvatarImage src={session?.user?.image ?? ""} />
-              <AvatarFallback>NA</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>
-              {session?.user?.name ? session?.user?.name : "My Account"}
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Sign Out</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+      <UserAvatar />
     </header>
   );
 }
---
+++ components/navbar/user-avatar.tsx
@@ -0,0 +1,57 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
+export function UserAvatar() {
+  //Get user data from session
+  const { data: session } = useSession();
+  return (
+    <div className="pr-4 hidden lg:flex">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar>
+            <AvatarImage src={session?.user?.image ?? ""} />
+            <AvatarFallback>NA</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>
+            {session?.user?.name ? session?.user?.name : "My Account"}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <Link href="/profile">
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+          </Link>
+          <DropdownMenuItem onClick={() => signOut()}>
+            Sign Out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
+
+export function MobileUserAvatar() {
+  //Get user data from session
+  const { data: session } = useSession();
+  return (
+    <div className="pt-4 flex flex-row items-center">
+      <Avatar>
+        <AvatarImage src={session?.user?.image ?? ""} />
+        <AvatarFallback>NA</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-col">
+        <p className="pl-4 text-[12px]">{session?.user?.name ?? "-"}</p>
+        <p className="pl-4 text-[12px]">{session?.user?.email ?? "-"}</p>
+      </div>
+    </div>
+  );
+}
