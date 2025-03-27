a06723efd7d894eb5d79aa73b5ab20ead4481dd7
Ioannis ZISAKIS <ioannis.zisakis@oracle.com>
2025-03-27 14:34:53 +0200

    feat: Hide navbar if is login page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 import Navbar from "@/components/navbar/navbar";
 import { Toaster } from "@/components/ui/toaster";
+import { usePathname } from "next/navigation";
 
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
@@ -18,12 +19,16 @@ export default function RootLayout({
 }>) {
   // Ensure the QueryClient persists across re-renders
   const [client] = useState(() => new QueryClient());
+  const pathname = usePathname();
+
+  //hide the navbar when user is not logged in
+  const isLoginPage = pathname === "/login";
   return (
     <SessionProvider>
       <html lang="en">
         <body>
           <QueryClientProvider client={client}>
-            <Navbar />
+            {!isLoginPage && <Navbar />}
             <main>{children}</main>
             <Toaster />
             <ReactQueryDevtools initialIsOpen={false} />
